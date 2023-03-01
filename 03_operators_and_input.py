b5411bcccb165e8f034717fd269a5cbe0bce0666
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-01 21:56:50 +0300

    Create 03_operators_and_input.py

---
+++ 03_operators_and_input.py
@@ -0,0 +1,22 @@
+# Operator and User Input
+
+"""
++ Addition
+- Subtraction
+* Multiplication
+/ Division
+% Mod
+** Exponentiation
+// Floor division
+"""
+
+x = 15
+y = 2
+
+print("Addition", x + y)
+print("Subtraction", x - y)
+print("Multiplication", x * y)
+print("Division", x / y)
+print("Mod", x % y)
+print("Exponentiation", x ** y)
+print("Floor division", x // y)
