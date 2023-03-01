bc68fa2708378b039c5c2038eb92bb34bc0b798c
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-01 21:56:47 +0300

    Create 02_dataTypes.py

---
+++ 02_dataTypes.py
@@ -0,0 +1,25 @@
+# Data Types
+
+"""
+int 10, 9, -2
+
+float 10.9, -10.9
+
+complex(10, 5)
+
+bool True False
+
+list []
+
+tuple ()
+
+dict {}
+"""
+
+number1 = 10
+number2 = 10.2
+boolean = True 
+text = "abcdef"
+array = [1,"a",False]
+tupleLisT = (1, True, "a")
+dictionary = {1: "A", 2: "B", 3: "C"}
