d78ab34ecd0bbd4f1bbd2d03ea592b17e247c4a3
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-04 16:45:39 +0300

    Update 05_ifelse_statements.py

--- 05_ifelse_statements.py
+++ 05_ifelse_statements.py
@@ -1,20 +1,27 @@
 # If Else Statements
 
-x = input("First number: ")
-y = input("Second number: ")
+# x = input("First number: ")
+# y = input("Second number: ")
 
-x = int(x)
-y = int(y)
+# x = int(x)
+# y = int(y)
 
-if x < y:
-    print("x is less than y!")
-    if x > 0:
-        print("x is a positive number.")
-    elif x == 0:
-        print("x is a neutral number.")
-    else:
-        print("x is a negative number.")
-elif x > y:
-    print("x is greater than y!")
-else:
-    print("x is equal to y!")
+# if x < y:
+#     print("x is less than y!")
+#     if x > 0:
+#         print("x is a positive number.")
+#     elif x == 0:
+#         print("x is a neutral number.")
+#     else:
+#         print("x is a negative number.")
+# elif x > y:
+#     print("x is greater than y!")
+# else:
+#     print("x is equal to y!")
+
+# "is" keyword
+
+# if type(10) is int:
+#     print("Yes, same object!")
+# else:
+#     print("No, not same object!")
