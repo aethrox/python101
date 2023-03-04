5e037eb6863cc3e7dd6bebd816a7624291318598
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-04 16:45:43 +0300

    Create 06_loops.py

---
+++ 06_loops.py
@@ -0,0 +1,32 @@
+# Loops
+
+# x = 0
+
+# while x < 10:
+#     x += 1
+#     print("Python!")
+
+# while x < 10:
+#     if x == 5:
+#         break # Keyword to terminate loops.
+#     x += 1
+#     print(x)
+
+# while x < 10:
+#     if x == 5:
+#         print("Five!")
+#         continue # Keyword to continue loops.
+#     x += 1
+#     print(x)
+
+# while x <= 10:
+#     if x == 5:
+#        print("Pass!")
+#        pass # Keyword to bypass the action.
+#     else:
+#         print(x)
+#     x += 1
+    
+# for x in range(0,10,2): #range(start number, end number, increment number)
+#     print("Python!")
+
