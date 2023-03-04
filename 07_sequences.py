bb3fbd7e832de05e715b9fa9d9273e0660bd74f3
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-04 16:45:45 +0300

    Create 07_sequences.py

---
+++ 07_sequences.py
@@ -0,0 +1,51 @@
+# Secuences
+
+# mylist = [10, 20, 30, "Text", True, 9.10]
+
+# print(mylist[3]) # Index: 3 > "Text"
+
+# print(mylist[1:3]) # Print elements 1 through 3
+
+# x = [1, 2, 3] 
+# y = [4, 5, 6, "hello", True]
+# j = [1,3,5,2,6,8,7]
+
+# print(x + y) # Connecting arrays
+
+# print(x * 4)
+
+# print(len(x))
+# print(max(x))
+# print(min(x))
+
+# x.append("New value")
+# x.insert(1, "New value")
+# x.remove(3)
+# x.pop(2)
+# print(x.index(3))
+
+# print(sorted(j))
+
+# "in" and "not in" keywords
+
+# print("hello" in y) # True
+# print("hello" not in y) # False
+
+# Tuple
+
+# y = [1, True, "False"]
+# x = (1, True, "False") # Unmutable !
+
+# x[0] = 2 # The data in tuple cannot be changed!
+
+# print(x, y)
+
+# Dictionary
+
+# x = {1: "N1", 2: "N2", 3: "N3"}
+
+# print(x.items())
+# print(x.keys())
+# print(x.values())
+# print(x[1])
+
