c4af5762c203ad870ddc0c045399e8d463f08f56
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-07 16:01:35 +0300

    Create 09_expectionHandling.py

---
+++ 09_expectionHandling.py
@@ -0,0 +1,25 @@
+# Expection Handling
+
+# try:
+#     x = int(input("First number: "))
+#     y = int(input("Second number: "))
+#     print(x / y)
+# # except:
+# #     print("There was an Error!")
+# except ValueError:
+#     print("Please enter a valid number next time!")
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
+# finally: 
+#     print("DONE!")
+
+# def some_function():
+#     if True: # condition
+#         # raise Exception("Something went terribly wrong!")
+#         raise ValueError("Please enter an item!")
+# some_function()
+
+# x = 100
+# y = 20
+
+# assert(x < y)
