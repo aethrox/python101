8aa9f2d777fc774bb321ba4341c81f38fa457a27
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-08 13:20:35 +0300

    Create 11_stringFunctions.py

---
+++ 11_stringFunctions.py
@@ -0,0 +1,36 @@
+# String Operations
+
+# text = "Python Beginner Tutorial"
+
+# print(len(text)) # Get string lenght
+# print(text[2]) # Get the character in index 2 of the string.
+
+"""
+\t tab
+\b backspace
+\s space
+"""
+
+# name = input()
+# age = int(input())
+
+# print("My name is %s and I am %d years old!" % (name, age))
+# print("My name is {} and I am {} years old!".format(name, age))
+
+# print(text.upper()) # Uppercase
+# print(text.lower()) # Lowercase
+# print(text.title()) # Title 
+# print(text.swapcase()) # Swapcase
+
+text = "I am Mike and my life is beautiful! Because of my job!"
+
+# print(text.count("my")) # Words or letter count
+
+# seperator = ","
+# mylist = ["Kitchen", "Dog", "Mike"]
+# print(seperator.join(mylist))
+
+# words = text.split(' ')
+# print(words)
+
+# print(text.replace('Mike', 'Bob'))
