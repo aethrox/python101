b72df6b8f27985c422fa0c98fa0c3165d2f0618b
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-03-28 21:11:21 +0300

    Create 18_socketServer.py

---
+++ 18_socketServer.py
@@ -0,0 +1,13 @@
+# Sockets and Network Programming
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("127.0.0.1", 4444))
+s.listen()
+
+while True:
+    client, address = s.accept()
+    print("Connected to {}".format(address))
+    client.send("You are connected!".encode())
+    client.close()
