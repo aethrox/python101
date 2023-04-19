51e423f1924525b2eff6dad6adfeac54c79b808a
Kaan D <69115453+1nnr3d@users.noreply.github.com>
2023-04-19 18:22:45 +0300

    Update 21_logging.py

--- 21_logging.py
+++ 21_logging.py
@@ -1,2 +1,28 @@
 # Logging
 import logging
+
+# Security Levels
+"""
+DEBUG
+INFO
+WARNING
+ERROR
+CRITICAL
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger("1nnr3d Logger")
+
+logger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler("mylog.log")
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(levelname)s - %(asctime)s: %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+logger.debug("This is a debug message!")
+logger.info("This is important information!")
